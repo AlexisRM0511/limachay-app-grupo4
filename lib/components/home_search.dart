188534d189992d09c7038b9a5a1098ad8fb717fb
BrandonMejia <shadowform-23@hotmail.com>
2022-06-25 13:40:12 -0500

    Integracion de elementos a Home Search.

--- lib/components/home_search.dart
+++ lib/components/home_search.dart
@@ -35,47 +35,23 @@ class HomeSearchPageState extends State<HomeSearchPage> {
   @override
   Widget build(BuildContext context){
     return Scaffold(
+        appBar: AppBar(
+          leading: const BackButton(
+            color: Colors.black,
+          ),
+          title: const Text('Buscar Delito'),
+          titleTextStyle: const TextStyle(
+            color: Colors.black,
+            fontSize: 27,
+            fontWeight: FontWeight.w500,
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
         body:Column(
           children: [
             const SingleChildScrollView(),
-            //Text("\n\n\n"),
-            Container(
-              width: 140.0,
-              height: 140.0,
-              padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
-              alignment: Alignment.bottomCenter,
-              child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Escudo_de_la_Polic%C3%ADa_Nacional_del_Per%C3%BA.png/1200px-Escudo_de_la_Polic%C3%ADa_Nacional_del_Per%C3%BA.png", width: 50),
-            ),
-            groupInput("Buscar", const Icon(Icons.search)),
-            Image.network("https://dirandro.policia.gob.pe/footer/4.png"),
-            Container(
-              child: const Text("¿Has Presenciado Algún Delito?",
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-            groupInput("Si o No", const Icon(Icons.check)),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              alignment: Alignment.bottomLeft,
-              child: const Text("Recientes",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,),
-              ),
-            ),
-            SizedBox(height: 20),
-            Expanded(
-                child: ListView(
-                  shrinkWrap: true,
-                  children: sueCases.map((sueCase) => SueCard(sueCase: sueCase)).toList(),
-                )
-            ),
-
+            groupInput("Buscar", const Icon(Icons.search))
           ],
         )
     );
